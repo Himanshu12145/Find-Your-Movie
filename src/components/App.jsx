43ae348ce79e061fc9c40aac08c38a30e426abb3
Himanshu12145 <himansu12145@gmail.com>
2023-02-05 12:56:10 +0530

    Basic router Structure

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,8 +1,21 @@
 import React from "react";
 import { CssBaseline } from "@mui/material";
+import { Route, Routes } from "react-router-dom";
+import { Actors, MovieInformation, Movies, NavBar, Profile } from "./";
 
-const App = () => {
-  return <div>Welcome to my application here on I will direct you all !!!</div>;
-};
+const App = () => (
+  <div>
+    <CssBaseline />
+    <NavBar />
+    <main>
+      <Routes>
+        <Route exact path="/movie/:id" element={<MovieInformation />} />
+        <Route exact path="/actors/:id" element={<Actors />} />
+        <Route exact path="/" element={<Movies />} />
+        <Route exact path="/profile/:id" element={<Profile />} />
+      </Routes>
+    </main>
+  </div>
+);
 
 export default App;
